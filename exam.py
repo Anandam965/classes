bc02869ed05fdfd617cd7af7dbb663912da50e21
Anandam Venkata Naga Sai <anandamvenkatanagasai@gmail.com>
2026-05-18 19:57:58 +0530

    Update exam.py

--- exam.py
+++ exam.py
@@ -4,8 +4,8 @@ import streamlit as st
 from supabase import create_client
 
 
-SUPABASE_URL = "https://ntmclisjmohkfpfigwjt.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50bWNsaXNqbW9oa2ZwZmlnd2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5ODU0NzQsImV4cCI6MjA5NDU2MTQ3NH0.bcm2hEBzCsEBklLKpBVvYGxXsGWNHHOZJOXx0w3YQBc"
+SUPABASE_URL = "https://lybhhtorasnwwaehqvnc.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5YmhodG9yYXNud3dhZWhxdm5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMDk0NTksImV4cCI6MjA5NDY4NTQ1OX0.8LO08tXBNBD83TIrR8oiuCIo97CtvvaupSIahTBAAuo"
 
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
